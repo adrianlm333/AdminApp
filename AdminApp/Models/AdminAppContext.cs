cfd9878dc30d5fdad8c9168c8d34bc58efdc5474
Adrían López <almorales@comebi.com.mx>
2024-12-17 18:08:46 -0600

    Se agrega coneccion a bd sqlite

---
+++ AdminApp/Models/AdminAppContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace AdminApp.Models
+{
+    public class AdminAppContext : DbContext
+    {
+        public AdminAppContext(DbContextOptions<AdminAppContext> options) : base(options) { }
+    }
+}
--- AdminApp/Program.cs
+++ AdminApp/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using AdminApp.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AdminAppContext>(options =>
+        options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
